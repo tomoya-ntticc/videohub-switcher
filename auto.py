61743427ed7ed7d13c2b6a006a7e32b2f29117a3
NTT ICC <ntticc@Mao-SHIBATA-iMac.local>
2022-12-10 19:37:20 +0900

    update streaming channel

--- auto.py
+++ auto.py
@@ -7,10 +7,11 @@ port = 9990
 sleeptime_seconds = 20
 timeout_seconds = 5
 input_channels = [5, 6, 7, 8, 9]
+streaming_channel = 11
 tn = telnetlib.Telnet(host, port, timeout_seconds)
 
 while True:
     channel = random.choice(input_channels)
-    tn.write((f"video output routing:\n10 {channel}\n\n").encode('ascii'))
+    tn.write((f"video output routing:\n{streaming_channel} {channel}\n\n").encode('ascii'))
     tn.read_until(b"ACK", timeout_seconds)
     time.sleep(sleeptime_seconds)
