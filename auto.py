98c1e20872c0773f68f84bcbb7031b5d7bfd4138
NTT ICC <ntticc@Mao-SHIBATA-iMac.local>
2022-12-10 19:09:25 +0900

    add auto switching

---
+++ auto.py
@@ -0,0 +1,16 @@
+import random
+import telnetlib
+import time
+
+host = "192.168.1.151"
+port = 9990
+sleeptime_seconds = 20
+timeout_seconds = 5
+input_channels = [5, 6, 7, 8, 9]
+tn = telnetlib.Telnet(host, port, timeout_seconds)
+
+while True:
+    channel = random.choice(input_channels)
+    tn.write((f"video output routing:\n10 {channel}\n\n").encode('ascii'))
+    tn.read_until(b"ACK", timeout_seconds)
+    time.sleep(sleeptime_seconds)
