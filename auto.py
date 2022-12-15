82894536027a710a52474d54f1706cee4b419dec
NTT ICC <ntticc@Mao-SHIBATA-iMac.local>
2022-12-15 16:23:50 +0900

    move comments

--- auto.py
+++ auto.py
@@ -22,8 +22,7 @@ tn = telnetlib.Telnet(host, port, timeout_seconds)
 
 async def switch_channels(output_channels = [4, 5], input_channels = [6, 7], sleeptime_seconds = 1):
     loop = asyncio.get_running_loop()
-    end_time = loop.time() + endtime_seconds
-    print(f"\n{datetime.datetime.now()} __ Start auto switching.")
+    end_time = loop.time() + endtime_seconds 
     while True:
         if (loop.time() + 1.0) >= end_time:
             break
@@ -32,12 +31,11 @@ async def switch_channels(output_channels = [4, 5], input_channels = [6, 7], sle
         for index, output_channel in enumerate(output_channels):
             tn.write((f"video output routing:\n{output_channel} {input_channels[index]}\n\n").encode('ascii'))
             tn.read_until(b"ACK", timeout_seconds)
-        print(f"{datetime.datetime.now()} __ switched channels, output:{output_channels}, input:{input_channels}")
+        print(f"{datetime.datetime.now()} __ Switched channels, output:{output_channels}, input:{input_channels}")
         await asyncio.sleep(sleeptime_seconds)
 
-    print(f"\n{datetime.datetime.now()} __ Finished auto switching.")
-
 async def main():
+    print(f"\n{datetime.datetime.now()} __ Start auto switching.\n")
     await asyncio.gather(
         # # for streaming
         switch_channels(streaming_output_channels, streaming_input_channels, 31),
@@ -46,6 +44,7 @@ async def main():
         # for landscape
         switch_channels(landscape_output_channels, landscape_input_channels, 47),
     )
+    print(f"\n{datetime.datetime.now()} __ Finished auto switching.")
 
 if __name__ == "__main__":
     asyncio.run(main())
